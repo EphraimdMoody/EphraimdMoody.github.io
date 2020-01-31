d07ddfb36f17cdb50d30b20dca1940bc691724a8
EphraimdMoody <60496255+EphraimdMoody@users.noreply.github.com>
2020-01-31 03:02:21 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,9 +3,12 @@ var Highscore = 0
 var inp = ''
 var mmbutt
 var loaded = 0
-var note
 var lbp
 var cnv
+var x
+var y
+var btnx
+var btny
 
 //mainmenu
 var tgbutt
@@ -25,8 +28,10 @@ function preload() {
 }
 
 function centerCanvas() {
-  var x = (windowWidth - width) / 2;
-  var y = (windowHeight - height) / 2;
+  x = (windowWidth - width) / 2;
+  y = (windowHeight - height) / 2;
+  btnx = (windowWidth - width) / 2;
+  btny = (windowHeight - height) * 1.4;
   cnv.position(x, y);
 }
 
@@ -44,8 +49,10 @@ function setup() {
   text('What grade are you in?', 90, 200);
   text('press a button below', 90, 250);
   fgbutt = createButton('First grade')
+  fgbutt.position(btnx,btny);
   fgbutt.mousePressed(fgsetup)
   tgbutt = createButton('Third grade')
+  tgbutt.position(btnx+90, btny);
   tgbutt.mousePressed(rgsetup)
   if(loaded == 1){
   dogbtn.remove()
@@ -67,27 +74,34 @@ function fgsetup(){
   fgbutt.remove()
   tgbutt.remove()
   dogbtn = createButton('dog');
+  dogbtn.position(btnx, btny);
   dogbtn.mousePressed(dogpress)
   catbtn = createButton('cat');
+  catbtn.position(btnx+45, btny);
   catbtn.mousePressed(catpress)
   bearbtn = createButton('bear');
+  bearbtn.position(btnx+86, btny);
   bearbtn.mousePressed(bearpress)
   applebtn = createButton('apple');
+  applebtn.position(btnx+136, btny);
   applebtn.mousePressed(applepress)
   loaded = 1
   mmbutt = createButton('Main Menu')
+  mmbutt.position(btnx+315, btny);
   mmbutt.mousePressed(setup)
 }
 
 function rgsetup(){
   createCanvas(400, 400);
   inp = createInput('Type obect name here');
+  inp.position(btnx, btny);
   inp.input(myInputEvent);
   tgbutt.remove()
   fgbutt.remove()
   loaded = 2
   randomObj();
   mmbutt = createButton('Main Menu')
+  mmbutt.position(btnx+315, btny);
   mmbutt.mousePressed(setup)
 }
 
